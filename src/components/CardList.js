5675e2242659d750eee85b9a5b482a3ff70c3c1f
kallelampinen <71073159+kallelampinen@users.noreply.github.com>
2021-03-18 10:38:07 +0100

    site rdy to deploy

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,35 +2,43 @@ import React from 'react';
 import Card from './card';
 
 
-const CardList = ({robots}) =>{
-  if(true){
-    throw new Error('nooo')
-  }
 
-  
+
+  const CardList = ({robots}) => {
+
+
+    // Manual error to display functionality of ErrorBoundry compoonent
+    
+    // if(true) {
+    //   throw new Error ('nooo');    
+    // }
+
     return(
 
-        <div>
-  {
-              robots.map((user, i) => {
+      <div>
+{
+            robots.map((user, i) => {
 
-                return(
-                  <Card 
-                  key= {i} 
-                  id={robots[i].id} 
-                  name={robots[i].name} 
-                  email={robots[i].email} 
-                  username={robots[i].username} 
-                  />
-                
+              return(
+                <Card 
+                key= {i} 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email} 
+                username={robots[i].username} 
+                />
+              
 
-            );
-            })
+          );
+          })
 
- }
-       
-      </div>
-    );
 }
+     
+    </div>
+  );
+
+  }
+
+
 
 export default CardList;
